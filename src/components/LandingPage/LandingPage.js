a0ae0f7f1a6fc7d302d8dddaa26b589e262383a8
TranDuc2411 <tranngocminhduc2411@gmail.com>
2022-07-16 19:50:30 +0700

    landing

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,7 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.css';
+import { 
+    Button,
+    Card,
+    CardBody,
+    CardTitle,
+    CardSubtitle,
+    CardText,
+    CardImg
+ } from 'reactstrap';
 export default function LadingPage(){
     return(
+            
         <div>
-            <h1>LadingPage</h1>
+          <h1>LandingPage</h1>
         </div>
+      
     )
 }
